ce610035172997e70053a40a9ea5b9e98aa397ae
Marius Oprea <dev.omr95@gmail.com>
2025-05-16 12:16:38 +0300

    Create Cognito & ApiGateway stacks

--- app.py
+++ app.py
@@ -5,16 +5,15 @@ from aws_cdk import App, Environment
 
 # Local imports
 from moderation_platform import (
-    get_conf_account_id, 
-    get_conf_region,
-    get_conf_repository_name,
-    get_conf_repository_owner,
-    get_conf_branch_name
+    get_conf_account_id, get_conf_region, get_conf_repository_name,
+    get_conf_repository_owner, get_conf_branch_name
 )
 from moderation_platform.observability_stack import ObservabilityStack
 from moderation_platform.s3_stack import S3Stack
 from moderation_platform.moderation_stack import ModerationStack
 from moderation_platform.cicd_stack import CICDStack
+from moderation_platform.cognito_stack import CognitoStack
+from moderation_platform.api_gateway_stack import ApiGatewayStack
 
 app = App()
 env_dev = Environment(
@@ -23,10 +22,13 @@ env_dev = Environment(
 )
 
 # Create stacks here
-print()
-
+cognito_stack = CognitoStack(app, 'CognitoStack', env=env_dev)
 observability_stack = ObservabilityStack(app, 'ObservabilityStack', env=env_dev)
 s3_stack = S3Stack(app, 'S3Stack', env=env_dev)
+
+api_gateway_stack = ApiGatewayStack(app, 'ApiGatewayStack', env=env_dev)
+api_gateway_stack.add_dependency(observability_stack)
+api_gateway_stack.add_dependency(cognito_stack)
 
 moderation_stack = ModerationStack(app, 'ModerationStack', env=env_dev)
 moderation_stack.add_dependency(observability_stack)
@@ -34,8 +36,8 @@ moderation_stack.add_dependency(s3_stack)
 
 # Create CI/CD stack
 cicd_stack = CICDStack(
-    app, 
-    'CICDStack', 
+    app,
+    'CICDStack',
     repository_name=get_conf_repository_name(context=app),
     owner=get_conf_repository_owner(context=app),
     branch_name=get_conf_branch_name(context=app),
